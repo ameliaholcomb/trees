0a59de96af7474e6e10ea09a47558fdb432acd6a
wangyijie6646 <yijiewang1337@gmail.com>
2020-05-25 02:14:10 -0400

    Created a new Android Project for the Tree Gallery. The application is able to access a storage file, read the images, and display them on a scrollable grid using recyclerView The image is scaled down to reduce memory consumption.

---
+++ TreeLabeler/app/src/main/java/com/trees/android/treelabeler/GalleryActivity.kt
@@ -0,0 +1,110 @@
+package com.trees.android.treelabeler
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.trees.android.treelabeler.databinding.GalleryActivityBinding
+import kotlinx.android.synthetic.main.gallery_activity.*
+import java.io.File
+
+class GalleryActivity : AppCompatActivity() {
+    val requestCode = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gallery_activity)
+        val adapter = GalleryImageAdapter()
+        val contentBinding: GalleryActivityBinding = DataBindingUtil.setContentView(
+            this, R.layout.gallery_activity)
+
+        // initializing grid
+        val manager = GridLayoutManager(this, 4)
+        contentBinding.rvGallery.layoutManager = manager
+        contentBinding.rvGallery.adapter = adapter
+
+        // path for storing data
+        val file = getAlbumStorageDir(this, "/Trees")
+
+        val dataList = mutableListOf<String>()
+
+        //Get permission for storage
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                // You can use the API that requires the permission.
+                // Just get images from storage for now
+                val projection = arrayOf(MediaStore.Images.ImageColumns.DATA)
+                applicationContext.contentResolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null
+                )?.use {cursor ->
+                    while (cursor.moveToNext()) {
+                        val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA)
+                        dataList.add(cursor.getString(dataColumn))
+                    }
+                }
+            }
+            else -> {
+                // You can directly ask for the permission.
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), requestCode)
+            }
+        }
+
+        adapter.data = dataList
+
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    //Check if external storage is available to read
+    fun isExternalStorageReadable(): Boolean {
+        return Environment.getExternalStorageState() in
+                setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
+    }
+
+    private fun getAlbumStorageDir(context: Context, albumName: String): File? {
+        // Get the pictures directory that's inside the app-specific directory on
+        // external storage.
+        val file = File(context.getExternalFilesDir(
+            Environment.DIRECTORY_PICTURES), albumName)
+        if (!file.mkdirs()) {
+            Log.e("IOError", "Directory not created")
+        }
+        return file
+    }
+}
---
+++ TreeLabeler/app/src/main/java/com/trees/android/treelabeler/GalleryImageAdapter.kt
@@ -0,0 +1,80 @@
+package com.trees.android.treelabeler
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class GalleryHolder(val imageView: ImageView) : RecyclerView.ViewHolder(imageView)
+
+class GalleryImageAdapter: RecyclerView.Adapter<GalleryHolder>() {
+    var data = listOf<String>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: GalleryHolder, position: Int) {
+        val item = data[position]
+
+        // Calculate the size of image and scale it down to reduce memory usage
+        val thumbnail = decodeSampledBitmapFromFile(item, 100, 100)
+
+        holder.imageView.setImageBitmap(thumbnail)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val placeholder = layoutInflater.inflate(
+            R.layout.gallery_grid_item, parent, false) as ImageView
+        return GalleryHolder(placeholder)
+    }
+
+    // Helper method to calculate smaller sample size for image thumbnails
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
+    }
+
+    // Decode bitmap from image file
+    private fun decodeSampledBitmapFromFile(
+        file: String,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Bitmap {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        return BitmapFactory.Options().run {
+            inJustDecodeBounds = true
+            BitmapFactory.decodeFile(file, this)
+
+            // Calculate inSampleSize
+            inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+
+            // Decode bitmap with inSampleSize set
+            inJustDecodeBounds = false
+
+            BitmapFactory.decodeFile(file, this)
+        }
+    }
+}
