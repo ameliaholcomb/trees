f8d4337c2b57774ceecd2b1ca96bc53539c39645
Amelia Holcomb <amelia.holcomb@gmail.com>
2022-05-02 18:17:45 +0100

    [fix]: view depth image only after capture

--- trees/app/src/main/python/improc.py
+++ trees/app/src/main/python/improc.py
@@ -2,16 +2,8 @@ import io
 import numpy as np
 from skimage import io as skio
 from skimage import img_as_ubyte, img_as_float, transform
-from scipy.ndimage import interpolation
+import matplotlib.pyplot as plt
 
-
-import processor
-
-
-# TODO these need to be changed to correct resolution
-# SHAPE = (360, 480)
-# TOF_SHAPE = (120, 160)
-# RGB_SHAPE = (640, 480)
 
 
 def run(depth_arr, rgb_arr, rgb_width, rgb_height, depth_width, depth_height):
@@ -22,35 +14,21 @@ def run(depth_arr, rgb_arr, rgb_width, rgb_height, depth_width, depth_height):
     print(f"TOF_SHAPE: {TOF_SHAPE}")
     print(f"RGB_SHAPE: {RGB_SHAPE}")
 
-    # Read images and resize so they can be directly overlaid.
+    # Read depth image and resize as if we were going to call processor.py
     depth = np.array(depth_arr, dtype=np.float64).reshape(TOF_SHAPE)
-    scale_factor = 2
-#     depth = np.kron(depth, np.ones((scale_factor, scale_factor)))
     depth = transform.resize(depth, RGB_SHAPE)
-    rgb = img_as_float(skio.imread(io.BytesIO(rgb_arr)))
 
-#     rgb = transform.resize(skio.imread(io.BytesIO(rgb_arr)), SHAPE)
+    # Instead of calling processor.py, just return the raw depth image for display.
 
-    try:
-        angle, left, right, est_depth, est_width = processor.process(depth, rgb)
-    except (processor.NoTrunkFoundError, processor.MissingDepthError) as e:
-        return bytes(img_as_ubyte(rgb)), 0, 0
-
-    # Prepare display image
-    bounds = np.zeros(RGB_SHAPE)
-    bounds[:, left] = 1
-    bounds[:, right] = 1
-    bounds_rot = interpolation.rotate(bounds, -angle, reshape=False)
-    rgb_disp = np.append(rgb, depth[:, :, np.newaxis], axis=2)
-    rgb_disp[np.where(np.abs(bounds_rot) > 0.1)] = [0.0, 1.0, 0.0, 1.0]
-
-    # img_rgbd_rotated = interpolation.rotate(rgbd, angle, reshape=False)
-    # rgb_disp = np.copy(img_rgbd_rotated)
-    # rgb_disp[:,left-1:left+1] = [1.0, 0.0, 0.0, 1.0]
-    # rgb_disp[:,right-1:right+1] = [1.0, 0.0, 0.0, 1.0]
-    # rgb_disp = interpolation.rotate(rgb_disp, -angle, reshape=False)
-    rgb_disp = np.clip(rgb_disp, 0, 1)
-
-    # TODO: pytype
-    # Java expects: byte[] displayImage, float estDepth, float estWidth
-    return bytes(img_as_ubyte(rgb_disp)), est_depth, est_width
+    # Convert depths to 0-255 integer range
+    norm = (255 * (depth - np.min(depth)) / np.ptp(depth)).astype(int)
+    # Use 0-255 integers to index into sequential colormap
+    c_arr = np.array(plt.cm.viridis.colors)
+    color = c_arr[norm]
+    # Add an axis of ones (alpha channel = 1)
+    # because Java expects an image of type RGB_8888
+    # (aka, four bytes per pixel)
+    ones = np.ones((color.shape[0], color.shape[1]))
+    color = np.append(color, ones[:, :, np.newaxis], axis=2)
+    # Return raw bytes and bogus depth and width values
+    return img_as_ubyte(color).tobytes(), 1, 1
